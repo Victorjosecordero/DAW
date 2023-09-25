34ff2b87abaf0cb45dd223446431c54616706c86
Victor Jose <vepncor@gmail.com>
2023-09-25 12:27:14 +0200

    c

--- uno.php
+++ uno.php
@@ -11,6 +11,24 @@
 
         $nom="clase";
         echo 'Hola mundo que tal  ' . $nom;
+        //comentario de una linea
+        #comentario de una linea
+        /*Comentario de bloque */
+        echo "<p>";
+        echo("esto es una función");
+        echo("</p>");
+
+        //CONSTANTES. Se escriben en mayusculas por consenso y no empienzan por $
+
+        define('PI',3.14);
+        echo "<p>" . PI . "</p>";
+
+        $radio= 3.5;
+        echo "<p> Area del circulo: " . 2*PI . "</p>";
+
+        const EURO= 166.86;
+
+        echo "<p>" . EURO . "</p>";
     
     ?>
 </body>
